f350365a99a359d75d1c1a787bf1bc065b4087d1
Shra1Yadav <93590332+Shra1Yadav@users.noreply.github.com>
2024-08-10 01:24:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    host: '0.0.0.0', // Allow external connections
+    port: 5173,      // Use port 5173
+  },
 })
