9735f8016be2afdf5f5074af956fd239139d71ee
Shra1Yadav <93590332+Shra1Yadav@users.noreply.github.com>
2024-08-10 12:23:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // src/index.js
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx'; // Ensure the correct file extension
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import StoreContextProvider from './Context/StoreContext';
+import StoreContextProvider from './Context/StoreContext.jsx'; // Ensure the correct file extension
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
